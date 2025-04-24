456ed37a8e211a9912d7d1fe72b090053ff1b655
Andrei <andreigota2003@gmail.com>
2025-04-24 18:11:26 +0300

    absolute imports on fe

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import tsconfigPaths from "vite-tsconfig-paths"
 
-// https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            src: "/src",
-        },
-    },
+    plugins: [react(), tsconfigPaths({ projects: ["tsconfig.app.json", "tsconfig.node.json"] })],
     server: {
         port: 3000,
     },
