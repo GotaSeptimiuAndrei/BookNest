81996aa7f11d0a8602e62958b8484e77f71ef62f
Andrei <andreigota2003@gmail.com>
2025-06-14 22:06:25 +0300

    quick fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
             <CssBaseline />
 
             <QueryClientProvider client={queryClient}>
-                <SnackbarProvider maxSnack={3} autoHideDuration={2500}>
+                <SnackbarProvider maxSnack={3} autoHideDuration={3000}>
                     <AuthProvider>
                         <BrowserRouter>
                             <Navbar />
